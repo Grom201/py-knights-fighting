5e88989e526ce3e75b6b45a1d241a18747eff918
Vitalii Vodolazkyi <vetal.vodolazkiy@gmail.com>
2026-04-24 12:11:05 +0200

    Looks like worked wariant

--- app/knight.py
+++ app/knight.py
@@ -1,18 +1,19 @@
 from equipment import armour, weapon, potion
 class Knight:
-    def __init__(self, name, power, hp, armour: armour, weapon: weapon, potion):
+    def __init__(self, name, power, hp, armour: armour, weapon: weapon, potion, protection):
         self.name = name
         self.power = power
         self.hp = hp
         self.armour = armour
         self.weapon = weapon
         self.potion = potion
+        self.protection = protection
 
     def apply_armour(self):
         # apply armour
-        self.armour.protection = 0
+        self.protection = 0
         for a in self.armour:
-            self.armour["protectiom"] += a["protection"]
+            self.protection += a["protection"]
 
     def apply_weapon(self):
         # apply weapon
@@ -21,11 +22,11 @@ class Knight:
     def apply_potion(self):
         # apply potion if exist
         if self.potion is not None:
-            if "power" in self.potion.effect:
-                self.power += self.potion.effect["power"]
+            if "power" in self.potion["effect"]:
+                self.power += self.potion["effect"]["power"]
 
-            if "protection" in self.potion.effect:
-                self.armour.protection += self.potion.effect["protection"]
+            if "protection" in self.potion["effect"]:
+                self.protection += self.potion["effect"]["protection"]
 
-            if "hp" in self.potion.effect:
-                self.hp += self.potion.effect["hp"]
+            if "hp" in self.potion["effect"]:
+                self.hp += self.potion["effect"]["hp"]
